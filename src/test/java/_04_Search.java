fce1891bf5c0f588440073209c4ae08755496299
semihcaglarr <caglarsemih1@outlook.com>
2024-05-15 08:47:32 +0300

    14.2 - 15.2 - 16.2 Test Cases of Postman API tests automation codes were written with Rest Assured

--- src/test/java/_04_Search.java
+++ src/test/java/_04_Search.java
@@ -1,5 +1,6 @@
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -11,10 +12,13 @@ public class _04_Search {
     String apiKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ODYxODNmYmE3MDMxMjM5NzM2Mzc5M2UyZTIzMDBiYyIsInN1YiI6IjY2MzUxZGVhOTU5MGUzMDEyY2JiOGQ5ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Anhzli1hmyToxEJmSVeTkzPM0fiPDui3xDha5S85AQM";
     String accessToken = "Bearer " + apiKey;
     RequestSpecification reqSpec;
+    int zombieProjectId;
+    int breakingBadId;
+    int mariaBadmajewId;
 
     @BeforeClass
     public void Setup() {
-        baseURI = "https://api.themoviedb.org/3/search"; // API_URL
+        baseURI = "https://api.themoviedb.org/3"; // API_URL
 
         reqSpec = new RequestSpecBuilder()
                 .addHeader("Authorization", accessToken)
@@ -27,61 +31,126 @@ public class _04_Search {
         System.out.println();
         System.out.println("------------TC_14_1_Search for Movie------------");
 
-        given()
-                .spec(reqSpec)
-                .param("query", "kutsal")
-                .log().uri()
+        Response returnData =
+                given()
+                        .spec(reqSpec)
+                        .param("query", "kutsal")
+                        .log().uri()
 
-                .when()
-                .get("/movie")
+                        .when()
+                        .get("/search/movie")
 
-                .then()
-                .log().body()
-                .statusCode(200);
+                        .then()
+                        //.log().body()
+                        .statusCode(200)
+                        .extract().response();
+
+        zombieProjectId = returnData.path("results[5].id");
+        System.out.println("zombieProjectId = " + zombieProjectId);
     }
 
     @Test(dependsOnMethods = "TC_14_1_SearchforMovie")
-    public void TC_14_2_SearchforTvShows() {
+    public void TC_14_2_GetMovieDetails() {
+        System.out.println();
+        System.out.println("------------TC_15_1_Get Movie Details------------");
+
+        given()
+                .spec(reqSpec)
+                .log().uri()
+
+                .when()
+                .get("movie/" + zombieProjectId)
+
+                .then()
+                .log().body()
+                .statusCode(200);
+    }
+
+    @Test
+    public void TC_15_1_SearchforTvShows() {
         System.out.println();
         System.out.println("------------TC_14_2_Search for Tv Shows------------");
 
+        breakingBadId =
+                given()
+                        .spec(reqSpec)
+                        .param("query", "bad")
+                        .log().uri()
+
+                        .when()
+                        .get("/search/tv")
+
+                        .then()
+                        // .log().body()
+                        .statusCode(200)
+                        .extract().response().path("results[0].id");
+
+        System.out.println("breakingBadId = " + breakingBadId);
+
+    }
+
+    @Test(dependsOnMethods = "TC_15_1_SearchforTvShows")
+    public void TC_15_2_GetTvShowsDetails() {
+        System.out.println();
+        System.out.println("------------TC_15_2_Get Tv Shows Details------------");
+
         given()
                 .spec(reqSpec)
-                .param("query", "bad")
                 .log().uri()
 
                 .when()
-                .get("/tv")
+                .get("tv/" + breakingBadId)
 
                 .then()
                 .log().body()
                 .statusCode(200);
-
     }
 
-    @Test(dependsOnMethods = "TC_14_2_SearchforTvShows")
-    public void TC_14_3_SearchforPersons() {
+    @Test
+    public void TC_16_1_SearchforPersons() {
         System.out.println();
         System.out.println("------------TC_14_3_Search for Persons------------");
 
+        mariaBadmajewId =
+                given()
+                        .spec(reqSpec)
+                        .param("query", "badem")
+                        .log().uri()
+
+                        .when()
+                        .get("/search/person")
+
+                        .then()
+                        //.log().body()
+                        .statusCode(200)
+                        .extract().jsonPath().getInt("results[18].id");
+
+        System.out.println("mariaBadmajewId = " + mariaBadmajewId);
+
+    }
+
+    @Test(dependsOnMethods = "TC_16_1_SearchforPersons")
+    public void TC_16_2_GetTvShowsDetails() {
+        System.out.println();
+        System.out.println("------------TC_16_2_Get Tv Shows Details------------");
+
         given()
                 .spec(reqSpec)
-                .param("query", "badem")
                 .log().uri()
 
                 .when()
-                .get("/person")
+                .get("person/" + mariaBadmajewId)
 
                 .then()
                 .log().body()
                 .statusCode(200);
-
     }
 
-    @Test(dependsOnMethods = "TC_14_3_SearchforPersons")
-    public void TC_14_4_SearchforKeywords() {
+
+    @Test
+    public void TC_16_3_SearchforKeywords() {
         System.out.println();
-        System.out.println("------------TC_14_4_Search for Keywords------------");
+        System.out.println("------------TC_16_3_Search for Keywords------------");
 
         given()
                 .spec(reqSpec)
@@ -89,12 +158,11 @@ public class _04_Search {
                 .log().uri()
 
                 .when()
-                .get("/keyword")
+                .get("/search/keyword")
 
                 .then()
-                .log().body()
+                //.log().body()
                 .statusCode(200);
     }
-
 
 }
